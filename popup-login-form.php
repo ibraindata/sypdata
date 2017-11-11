d24fd7e719df4b77d7764df76ff0fd3e02bb2d76
synapticasoftware <synapticasoftware@gmail.com>
2017-11-11 03:32:38 -0800

    push all files

---
+++ popup-login-form.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying a login popup form.
+ * This form is loaded in footer for non-logged-in user for easy access.
+ *
+ * @package Listify
+ * @since 2.3.0
+ * @version 2.3.0
+ */
+?>
+
+<?php if ( listify_has_integration( 'woocommerce' ) ) : ?>
+
+	<div id="listify-login-popup" class="popup<?php if ( get_option( 'woocommerce_enable_myaccount_registration' ) === 'yes' ) : ?> popup-wide<?php endif; ?>">
+
+		<?php get_template_part( 'woocommerce/myaccount/form-login' );?>
+
+	</div>
+
+<?php else : ?>
+
+	<div id="listify-login-popup" class="popup">
+
+		<h2 class="popup-title"><?php echo esc_html( get_theme_mod( 'content-login-title', __( 'Login', 'listify' ) ) ); ?></h2>
+
+		<?php listify_login_form(); ?>
+
+	</div>
+
+<?php endif; ?>
+
